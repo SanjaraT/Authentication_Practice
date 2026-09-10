d6440a0acfb7339acb01bb00fdbc87ef183679b2
sanjaraT <sanjaratunola@gmail.com>
2026-09-10 20:59:27 +0600

    Stage 1: add signup and login endpoints

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from supabase import create_client
+from pydantic import BaseModel
 import os
+
 
 load_dotenv()
 
@@ -11,5 +13,27 @@ SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 app = FastAPI(title="Auth API")
+class AuthReq(BaseModel):
+    email: str
+    password : str
+
+    
+@app.post("/signup")
+def signup(request: AuthReq):
+    response = supabase.auth.sign_up({
+        "email": request.email,
+        "password": request.password
+    })
+
+    return response
+
+@app.post("/login")
+def login(request: AuthReq):
+    response = supabase.auth.sign_in_with_password({
+        "email": request.email,
+        "password": request.password
+    })
+
+    return response
 
 print("Server running and connected to Supabase")
