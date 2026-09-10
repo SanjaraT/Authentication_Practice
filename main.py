cd6888cf0e4d7e5120e81c07ffb37be89effc925
sanjaraT <sanjaratunola@gmail.com>
2026-09-10 20:36:01 +0600

    Stage 0: setup server and supabase client

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+from supabase import create_client
+import os
+
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+app = FastAPI(title="Auth API")
+
+print("Server running and connected to Supabase")
